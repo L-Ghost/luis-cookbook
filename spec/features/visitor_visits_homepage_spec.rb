d9e927fac1ede73b233966f40622c3248a4b381e
L-Ghost <luis.arantes.sp@gmail.com>
2019-02-24 17:22:41 -0300

    Add first tests - home page
      add tests to verify if home page content can be viewed, and
    if previously registered recipe data can also be seen

---
+++ spec/features/visitor_visits_homepage_spec.rb
@@ -0,0 +1,39 @@
+require 'rails_helper'
+
+feature 'Visitor visits homepage' do
+  
+  scenario 'successfully' do
+    visit root_path
+
+    expect(page).to have_css('h1', text: 'CookBook')
+    expect(page).to have_css('p', text: 'Bem-vindo ao maior livro de receitas online')
+  end
+
+  scenario 'and view recipes list' do
+    # dados de receitas para testar pagina inicial
+    recipe = Recipe.create(title: 'Pão de Queijo de Beterraba', difficulty: 'Médio',
+        recipe_type: 'Entrada', cuisine: 'Brasileira', cook_time: 40,
+        ingredients: '1 batata média cozida, 1 beterraba cozida, 1/2 xícara de polvilho azedo, 2 colheres de sopa de azeite, 1 olher de chá de sal, 1/2 colher de levedo de cerveja (opcional)',
+        cook_method: 'Amasse a batata e a beterraba ainda quentes até quase virar um purê. Em seguida adicione o azeite, levedo de cerveja, sal e misture bem. Adicione o polvilho azedo e o doce...')
+    recipe2 = Recipe.create(title: 'Peixinho da Horta', difficulty: 'Fácil',
+        recipe_type: 'Prato Principal', cuisine: 'Portuguesa', cook_time: 30,
+        ingredients: 'Folhas de Stachys Bizantina, 1/4  de xícara de farinha de trigo, 1/4 de xícara de fubá...',
+        cook_method: 'Lave bem as folhas do peixinho da horta e seque-as muito bem com um pano de prato limpo ou papel toalha...')
+    
+    visit root_path
+    
+    # dados de receitas a serem vistos na pagina inicial
+    expect(page).to have_css('h1', text: recipe.title)
+    expect(page).to have_css('li', text: recipe.recipe_type)
+    expect(page).to have_css('li', text: recipe.cuisine)
+    expect(page).to have_css('li', text: recipe.difficulty)
+    expect(page).to have_css('li', text: "#{recipe.cook_time} minutos")
+
+    expect(page).to have_css('h1', text: recipe2.title)
+    expect(page).to have_css('li', text: recipe2.recipe_type)
+    expect(page).to have_css('li', text: recipe2.cuisine)
+    expect(page).to have_css('li', text: recipe2.difficulty)
+    expect(page).to have_css('li', text: "#{recipe2.cook_time} minutos")
+  end
+  
+end
