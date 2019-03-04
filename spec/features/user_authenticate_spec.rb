452f7ba4aa7fa27ec7cfead2eec17b9e27440df1
L-Ghost <luis.arantes.sp@gmail.com>
2019-03-04 14:53:31 -0300

    Add login and logout navigation tests
      tests which make Capybara interact with login/logout links

--- spec/features/user_authenticate_spec.rb
+++ spec/features/user_authenticate_spec.rb
@@ -18,6 +18,36 @@ feature 'User Authenticate' do
     expect(page).to have_link('Logout')
   end
 
+  scenario 'successfully' do
+    user = setup_user
+    visit root_path
+
+    click_on 'Login'
+    fill_in 'Email', with: user.email
+    fill_in 'Senha', with: user.password
+    within 'form' do
+      click_on 'Login'
+    end
+    
+    expect(current_path).to eq(root_path)
+    expect(page).not_to have_link('Login')
+    expect(page).to have_link('Logout')
+    expect(page).to have_css('p', text: "Logado como #{user.email}")
+  end
+
+  scenario 'and logout' do
+    user = setup_user
+    login_as(user, scope: :user)
+    visit root_path
+
+    click_on 'Logout'
+
+    expect(current_path).to eq(root_path)
+    expect(page).to have_link('Login')
+    expect(page).not_to have_link('Logout')
+    expect(page).not_to have_css('p', text: "Logado como #{user.email}")
+  end
+
   def setup_user
     User.create!(email: 'emailtest@cookbook.com', password: 't3stp4ssw0rd')
   end
