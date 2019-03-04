98b8cf58bbece563bc39ad6f1213f878fd00e5b6
L-Ghost <luis.arantes.sp@gmail.com>
2019-03-04 14:14:16 -0300

    Add Authentication / Devise
      with model User

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -58,4 +58,6 @@ Rails.application.configure do
 
   # Store files locally.
   config.active_storage.service = :local
+  # authentication
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 end
