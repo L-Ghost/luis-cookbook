14adab98d6c37a3cee15c816a59c2c3d04e6a0d2
L-Ghost <luis.arantes.sp@gmail.com>
2019-03-04 23:14:53 -0300

    Do not allow user to delete recipes of another user

--- spec/features/user_delete_recipe_spec.rb
+++ spec/features/user_delete_recipe_spec.rb
@@ -32,5 +32,34 @@ feature 'User delete recipe' do
     expect(page).not_to have_css('h1', text: 'Pão de Queijo de Beterraba')
     expect(page).to have_css('h1', text: 'Peixinho da Horta')
   end
+
+  scenario 'only if the recipe belongs to the user' do
+    # dados para o teste
+    user = User.create!(email: 'emailtest@cookbook.com', password: 't3stp4ssw0rd')
+    another_user = User.create!(email: 'emailtest2@cookbook.com', password: 't3stp4ssw0rd2')
+    
+    entrada = RecipeType.create(name: 'Entrada')
+    prato_principal = RecipeType.create(name: 'Prato Principal')
+
+    cuisine_br = Cuisine.create(name: 'Brasileira')
+    cuisine_pt = Cuisine.create(name: 'Portuguesa')
+
+    recipe = Recipe.create(title: 'Pão de Queijo de Beterraba', difficulty: 'Médio',
+        recipe_type: entrada, cuisine: cuisine_br, cook_time: 40,
+        ingredients: '1 batata média cozida, 1 beterraba cozida, 1/2 xícara de polvilho azedo, 2 colheres de sopa de azeite, 1 olher de chá de sal, 1/2 colher de levedo de cerveja (opcional)',
+        cook_method: 'Amasse a batata e a beterraba ainda quentes até quase virar um purê. Em seguida adicione o azeite, levedo de cerveja, sal e misture bem. Adicione o polvilho azedo e o doce...',
+        user: user)
+    another_recipe = Recipe.create(title: 'Peixinho da Horta', difficulty: 'Fácil',
+        recipe_type: prato_principal, cuisine: cuisine_pt, cook_time: 30,
+        ingredients: 'Folhas de Stachys Bizantina, 1/4 de xícara de farinha de trigo, 1/4 de xícara de fubá...',
+        cook_method: 'Lave bem as folhas do peixinho da horta e seque-as muito bem com um pano de prato limpo ou papel toalha...',
+        user: another_user)
+    
+    login_as(user, scope: :user)
+    visit root_path
+    click_on another_recipe.title
+
+    expect(page).not_to have_link('Deletar')
+  end
   
 end
