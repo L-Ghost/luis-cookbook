10a9b15917c886f39dd9f846c1d50fe0b1fbad0e
L-Ghost <luis.arantes.sp@gmail.com>
2019-03-08 15:18:28 -0300

    Add list viewing tests
      new spec file created

---
+++ spec/features/user_view_its_lists_spec.rb
@@ -0,0 +1,36 @@
+require 'rails_helper'
+
+feature 'User view its lists' do
+  scenario 'through root, but needs to be authenticated' do
+    visit root_path
+
+    expect(page).not_to have_link('Minhas Listas')
+  end
+
+  scenario 'but needs to be authenticated' do
+    visit my_lists_path
+
+    expect(current_path).to eq(new_user_session_path)
+  end
+
+  scenario 'successfully' do
+    setup_data
+    visit root_path
+    click_on 'Minhas Listas'
+    
+    expect(current_path).to eq(my_lists_path)
+    expect(page).to have_css('h1', text: 'Minhas Listas')
+    expect(page).to have_link('Tortas')
+    expect(page).to have_link('Doces')
+    expect(page).to have_link('Festa Junina')
+    expect(page).to have_css('h2', text: 'Total de Listas: 3')
+  end
+  
+  def setup_data
+    user = User.create!(email: 'chicobento@tdm.com', password: 'chicao2018')
+    List.create!(name: 'Tortas', user: user)
+    List.create!(name: 'Doces', user: user)
+    List.create!(name: 'Festa Junina', user: user)
+    login_as(user, scope: :user)
+  end
+end
